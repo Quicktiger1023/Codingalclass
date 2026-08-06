54cc74fd039e830e13561b7c4dd762a49ec87f2e
Quicktiger1023 <arnavm911@gmail.com>
2026-08-06 18:52:06 +0530

    commit message

---
+++ lesson11/act.py
@@ -0,0 +1,26 @@
+print("####################################################################")
+print("                welcome to the chore checklist countdown           ")
+print("####################################################################")
+
+totalforday=5
+count=totalforday;
+print("today there are{count} chores to be done.")
+
+comp=0
+ctcomp= 1
+
+while ctcomp <=totalforday:
+    if ctcomp==1:
+        print("do the morning rituals on time")
+
+    elif ctcomp==2:
+        print("quickly finish ur breakfast on time")
+
+    elif ctcomp==3:
+        print("reach the assembly of the school ")
+
+    if ctcomp==2:
+        print("complete homework")
+
+    elif ctcomp==2:
+        print("attend tution")
