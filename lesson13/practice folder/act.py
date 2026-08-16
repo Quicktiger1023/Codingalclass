21f836ff15a901416f5b0a1331b270debb770af6
Quicktiger1023 <arnavm911@gmail.com>
2026-08-16 17:11:28 +0400

    hello world

--- lesson13/practice folder/act.py
+++ lesson13/practice folder/act.py
@@ -1,3 +1,7 @@
 n= int(input("enter the number of rows in the rows:     "))
-for i in range(n):
-    print("#"*n)
+
+for i in range(n):#it means for will here go from 0 to n-1
+    for j in range(i): # if n=5,then for will go from 0 to 4
+        #i and j here are just an iterater which keeps changing its value according to range
+        print("#", end=" ")
+    print(  )
